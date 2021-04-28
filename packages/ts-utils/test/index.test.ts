9f9d189c82e17b7cb77ebf098bd8ee0954c59460
Wahidyan Kresna Fridayoka <wahidyankf@gmail.com>
2021-04-28 14:52:11 +0000

    [ts-utils] Add test for index.ts

--- packages/ts-utils/test/index.test.ts
+++ packages/ts-utils/test/index.test.ts
@@ -1,3 +1,14 @@
-test('test', () => {
-  expect(true).toEqual(true);
+import * as utils from '../src/index';
+
+describe('All utils are exported', () => {
+  test('option utils are exported', () => {
+    expect(utils).toHaveProperty('option');
+    expect(utils.option).toHaveProperty('some');
+    expect(utils.option).toHaveProperty('none');
+    expect(utils.option).toHaveProperty('isSome');
+    expect(utils.option).toHaveProperty('isNone');
+    expect(utils.option).toHaveProperty('map');
+    expect(utils.option).toHaveProperty('flatMap');
+    expect(utils.option).toHaveProperty('getOrElse');
+  });
 });
