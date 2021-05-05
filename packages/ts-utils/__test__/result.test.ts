6c67bcf3a1795af2aaf2e68a79b50d56b2f1ace3
Wahidyan Kresna Fridayoka <wahidyankf@gmail.com>
2021-05-05 08:18:56 +0700

    [ts-utils] Feature - Result Utils
    * [ts-utils] Result Type - Init
    
    * [ts-utils] Result Type - export result methods
    
    * [ts-utils] Result Type - add ok function
    
    * [ts-utils] Result Type - add error function
    
    * [ts-utils] Result Type - add isOk function
    
    * [ts-utils] Result Type - add isError function
    
    * [ts-utils] Result Type - refactor
    
    * [ts-utils] Result Type - add line break between functions
    
    * [ts-utils] Result Type - refactor (put 'get' before 'map' functions)
    
    * [ts-utils] Result Type - add get function
    
    * [ts-utils] Result Type - refactor test
    
    * [ts-utils] Result Type - add getError functions
    
    * [ts-utils] Result Type - add getOrElse function
    
    * [ts-utils] Result Type - add getErrorOrElse function
    
    * [ts-utils] Result Type - add map function
    
    * [ts-utils] Result Type - add mapError function
    
    * [ts-utils] Result Type - add flatMap function
    
    * [ts-utils] Result Type - add flatMapError function

---
+++ packages/ts-utils/__test__/result.test.ts
@@ -0,0 +1,207 @@
+import {
+  ok,
+  error,
+  isOk,
+  isError,
+  get,
+  getOrElse,
+  getError,
+  getErrorOrElse,
+  map,
+  mapError,
+  flatMap,
+  flatMapError,
+} from '../src/result';
+import {some, none} from '../src/option';
+
+describe('ok works correctly', () => {
+  test('ok generate tag correctly', () => {
+    expect(ok('string')).toHaveProperty('_tag');
+    expect(ok('string')).toHaveProperty('value');
+  });
+
+  test('ok generate value correctly', () => {
+    expect(ok('string')).toEqual({_tag: 'Ok', value: 'string'});
+    expect(ok(1)).toEqual({_tag: 'Ok', value: 1});
+  });
+});
+
+describe('error works correctly', () => {
+  test('error generate tag correctly', () => {
+    expect(error('string')).toHaveProperty('_tag');
+    expect(error('string')).toHaveProperty('value');
+  });
+
+  test('error generate value correctly', () => {
+    expect(error('string')).toEqual({_tag: 'Error', value: 'string'});
+    expect(error(1)).toEqual({_tag: 'Error', value: 1});
+  });
+});
+
+describe('isOk works correctly', () => {
+  test('isOk returns true on ok', () => {
+    expect(isOk(ok('string'))).toEqual(true);
+    expect(isOk(ok(1))).toEqual(true);
+  });
+
+  test('isOk returns false on error', () => {
+    expect(isOk(error('string'))).toEqual(false);
+    expect(isOk(error(1))).toEqual(false);
+  });
+});
+
+describe('isError works correctly', () => {
+  test('isError returns true on error', () => {
+    expect(isError(error('string'))).toEqual(true);
+    expect(isError(error(1))).toEqual(true);
+  });
+
+  test('isError returns false on ok', () => {
+    expect(isError(ok('string'))).toEqual(false);
+    expect(isError(ok(1))).toEqual(false);
+  });
+});
+
+describe('get works correctly', () => {
+  test('get returns some value on ok', () => {
+    expect(get(ok('string'))).toEqual(some('string'));
+    expect(get(ok(1))).toEqual(some(1));
+  });
+
+  test('get returns none on error', () => {
+    expect(get(error('string'))).toEqual(none());
+    expect(get(error(1))).toEqual(none());
+  });
+});
+
+describe('getError works correctly', () => {
+  test('getError returns some value on error', () => {
+    expect(getError(error('string'))).toEqual(some('string'));
+    expect(getError(error(1))).toEqual(some(1));
+  });
+
+  test('getError returns none on ok', () => {
+    expect(getError(ok('string'))).toEqual(none());
+    expect(getError(ok(1))).toEqual(none());
+  });
+});
+
+describe('getOrElse works correctly', () => {
+  test('getOrElse returns the value on ok', () => {
+    expect(getOrElse('default')(ok('string'))).toEqual('string');
+    expect(getOrElse(0)(ok(1))).toEqual(1);
+  });
+
+  test('getOrElse returns the default value on error', () => {
+    expect(getOrElse('default')(error('string'))).toEqual('default');
+    expect(getOrElse(0)(error(1))).toEqual(0);
+  });
+});
+
+describe('getErrorOrElse works correctly', () => {
+  test('getErrorOrElse returns the value on error', () => {
+    expect(getErrorOrElse('default')(error('string'))).toEqual('string');
+    expect(getErrorOrElse(0)(error(1))).toEqual(1);
+  });
+
+  test('getErrorOrElse returns the default value on ok', () => {
+    expect(getErrorOrElse('default')(ok('string'))).toEqual('default');
+    expect(getErrorOrElse(0)(ok(1))).toEqual(0);
+  });
+});
+
+describe('map works correctly', () => {
+  test('map successfully maps the ok value', () => {
+    expect(map((s: string) => 'hello ' + s)(ok('string'))).toEqual({
+      _tag: 'Ok',
+      value: 'hello string',
+    });
+    expect(map((x: number) => x * 2)(ok(1))).toEqual({
+      _tag: 'Ok',
+      value: 1 * 2,
+    });
+  });
+  test('map skips the operation on error', () => {
+    expect(map((s: string) => 'hello ' + s)(error('string'))).toEqual({
+      _tag: 'Error',
+      value: 'string',
+    });
+    expect(map((x: number) => x * 2)(error(1))).toEqual({
+      _tag: 'Error',
+      value: 1,
+    });
+  });
+});
+
+describe('mapError works correctly', () => {
+  test('mapError successfully maps the error value', () => {
+    expect(mapError((s: string) => 'hello ' + s)(error('string'))).toEqual({
+      _tag: 'Error',
+      value: 'hello string',
+    });
+    expect(mapError((x: number) => x * 2)(error(1))).toEqual({
+      _tag: 'Error',
+      value: 1 * 2,
+    });
+  });
+  test('mapError skips the operation on ok', () => {
+    expect(mapError((s: string) => 'hello ' + s)(ok('string'))).toEqual({
+      _tag: 'Ok',
+      value: 'string',
+    });
+    expect(mapError((x: number) => x * 2)(ok(1))).toEqual({
+      _tag: 'Ok',
+      value: 1,
+    });
+  });
+});
+
+describe('flatMap works correctly', () => {
+  test('flatMap successfully maps the ok value', () => {
+    expect(flatMap((s: string) => ok('hello ' + s))(ok('string'))).toEqual({
+      _tag: 'Ok',
+      value: 'hello string',
+    });
+    expect(flatMap((x: number) => ok(x * 2))(ok(1))).toEqual({
+      _tag: 'Ok',
+      value: 1 * 2,
+    });
+  });
+  test('flatMap skips the operation on error', () => {
+    expect(flatMap((s: string) => ok('hello ' + s))(error('string'))).toEqual({
+      _tag: 'Error',
+      value: 'string',
+    });
+    expect(flatMap((x: number) => ok(x * 2))(error(1))).toEqual({
+      _tag: 'Error',
+      value: 1,
+    });
+  });
+});
+
+describe('flatMapError works correctly', () => {
+  test('flatMapError successfully maps the error value', () => {
+    expect(
+      flatMapError((s: string) => error('hello ' + s))(error('string')),
+    ).toEqual({
+      _tag: 'Error',
+      value: 'hello string',
+    });
+    expect(flatMapError((x: number) => error(x * 2))(error(1))).toEqual({
+      _tag: 'Error',
+      value: 1 * 2,
+    });
+  });
+  test('flatMapError skips the operation on ok', () => {
+    expect(
+      flatMapError((s: string) => error('hello ' + s))(ok('string')),
+    ).toEqual({
+      _tag: 'Ok',
+      value: 'string',
+    });
+    expect(flatMapError((x: number) => error(x * 2))(ok(1))).toEqual({
+      _tag: 'Ok',
+      value: 1,
+    });
+  });
+});
---
+++ packages/ts-utils/src/result.ts
@@ -0,0 +1,128 @@
+import option, {none, Option} from './option';
+
+interface Ok<T> {
+  readonly _tag: 'Ok';
+  readonly value: T;
+}
+
+interface Error<T> {
+  readonly _tag: 'Error';
+  readonly value: T;
+}
+
+export type Result<T, U> = Ok<T> | Error<U>;
+
+export function ok<T, U>(value: T): Result<T, U> {
+  return {_tag: 'Ok', value};
+}
+
+export function error<T, U>(value: U): Result<T, U> {
+  return {_tag: 'Error', value};
+}
+
+export function isOk<T, U>(result: Result<T, U>): boolean {
+  return result._tag === 'Ok';
+}
+
+export function isError<T, U>(result: Result<T, U>): boolean {
+  return result._tag === 'Error';
+}
+
+export function get<T, U>(result: Result<T, U>): Option<T> {
+  if (result._tag === 'Ok') {
+    return option.some(result.value);
+  } else {
+    return option.none();
+  }
+}
+
+export function getError<T, U>(result: Result<T, U>): Option<U> {
+  if (result._tag === 'Error') {
+    return option.some(result.value);
+  } else {
+    return option.none();
+  }
+}
+
+export function getOrElse<T, U>(value: T): (result: Result<T, U>) => T {
+  return (result: Result<T, U>): T => {
+    if (result._tag === 'Ok') {
+      return result.value;
+    } else {
+      return value;
+    }
+  };
+}
+
+export function getErrorOrElse<T, U>(value: U): (result: Result<T, U>) => U {
+  return (result: Result<T, U>): U => {
+    if (result._tag === 'Error') {
+      return result.value;
+    } else {
+      return value;
+    }
+  };
+}
+
+export function map<T, U, T1>(
+  fn: (value: T) => T1,
+): (result: Result<T, U>) => Result<T1, U> {
+  return (result) => {
+    if (result._tag === 'Ok') {
+      return ok(fn(result.value));
+    } else {
+      return result as Result<T1, U>;
+    }
+  };
+}
+
+export function mapError<T, U, U1>(
+  fn: (value: U) => U1,
+): (result: Result<T, U>) => Result<T, U1> {
+  return (result) => {
+    if (result._tag === 'Error') {
+      return error(fn(result.value));
+    } else {
+      return result as Result<T, U1>;
+    }
+  };
+}
+
+export function flatMap<T, U, T1>(
+  fn: (value: T) => Result<T1, U>,
+): (result: Result<T, U>) => Result<T1, U> {
+  return (result: Result<T, U>) => {
+    if (result._tag === 'Ok') {
+      return fn(result.value);
+    } else {
+      return result as Result<T1, U>;
+    }
+  };
+}
+
+export function flatMapError<T, U, U1>(
+  fn: (value: U) => Result<T, U1>,
+): (result: Result<T, U>) => Result<T, U1> {
+  return (result: Result<T, U>) => {
+    if (result._tag === 'Error') {
+      return fn(result.value);
+    } else {
+      return result as Result<T, U1>;
+    }
+  };
+}
+
+export default {
+  ok,
+  error,
+  isOk,
+  isError,
+  get,
+  getError,
+  getOrElse,
+  getErrorOrElse,
+  map,
+  mapError,
+  flatMap,
+  flatMapError,
+};
