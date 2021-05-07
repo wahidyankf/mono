62fab94d88ed948cc327922a07471cfda3f6e433
Wahidyan Kresna Fridayoka <wahidyankf@gmail.com>
2021-05-07 01:00:32 +0000

    [ts-utils] Feature - Attempt - Add custom message key, test ok and error result

--- packages/ts-utils/__test__/attempt.test.ts
+++ packages/ts-utils/__test__/attempt.test.ts
@@ -1,12 +1,52 @@
 import {attempt} from '../src/attempt';
+import {ok, error} from '../src/result';
+import {some} from '../src/option';
 
 describe('attempt works correctly', () => {
-  test('attempt generate tags correctly', () => {
+  test('attempt generates tags correctly', () => {
     expect(attempt(() => eval('null.test'))).toHaveProperty('_tag');
     expect(attempt(() => eval('null.test'))).toHaveProperty('value');
-    expect(attempt(() => 1 / 0)).toHaveProperty('_tag');
-    expect(attempt(() => 1 / 0)).toHaveProperty('value');
-    expect(attempt(() => 'hello')).toHaveProperty('_tag');
-    expect(attempt(() => 'hello')).toHaveProperty('value');
+    expect(attempt(() => 'str')).toHaveProperty('_tag');
+    expect(attempt(() => 'str')).toHaveProperty('value');
+  });
+
+  test('attempt correctly returns successful result', () => {
+    expect(attempt(() => 'str')).toEqual(ok('str'));
+    expect(attempt(() => 1)).toEqual(ok(1));
+    expect(attempt(() => some('str'))).toEqual(ok(some('str')));
+    expect(attempt(() => 1 / 0)).toEqual(ok(Infinity));
+    expect(attempt(() => {})).toEqual(ok(undefined));
+  });
+
+  test('attempt correctly returns error result on default error message key', () => {
+    expect(attempt(() => eval('null.test'))).toEqual(
+      error("Cannot read property 'test' of null"),
+    );
+    expect(
+      attempt(() => {
+        throw 'some error message';
+      }),
+    ).toEqual(error('some error message'));
+    expect(
+      attempt(() => {
+        throw {message: 'some error message'};
+      }),
+    ).toEqual(error('some error message'));
+  });
+
+  test('attempt correctly returns error result on custom error message key', () => {
+    expect(attempt(() => eval('null.test'), 'description')).toEqual(
+      error('{}'),
+    );
+    expect(
+      attempt(() => {
+        throw 'some error message';
+      }, 'description'),
+    ).toEqual(error('some error message'));
+    expect(
+      attempt(() => {
+        throw {description: 'some error message'};
+      }, 'description'),
+    ).toEqual(error('some error message'));
   });
 });
--- packages/ts-utils/src/attempt.ts
+++ packages/ts-utils/src/attempt.ts
@@ -1,15 +1,20 @@
 import {ok, error, Result} from './result';
 
-export function attempt<T>(f: () => T): Result<T, string> {
+export function attempt<T>(
+  f: () => T,
+  errorMessageKey: string = 'message',
+): Result<T, string> {
   try {
     return ok(f());
-  } catch (e) {
-    if (e.message) {
-      return typeof e.message === 'string'
-        ? error(e.message)
-        : error(JSON.stringify(e));
+  } catch (err) {
+    const errorMessage = err[errorMessageKey];
+
+    if (errorMessage) {
+      return typeof errorMessage === 'string'
+        ? error(errorMessage)
+        : error(JSON.stringify(err));
     } else {
-      return typeof e === 'string' ? error(e) : error(JSON.stringify(e));
+      return typeof err === 'string' ? error(err) : error(JSON.stringify(err));
     }
   }
 }
