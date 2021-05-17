e9bc94288af5145486fca9718ef30f1d6fba9f62
Wahidyan Kresna Fridayoka <wahidyankf@gmail.com>
2021-05-17 12:33:48 +0000

    [ts-utils] add test for match

--- packages/ts-utils/__test__/adt.test.ts
+++ packages/ts-utils/__test__/adt.test.ts
@@ -2,7 +2,43 @@ import {result, option, adt, Option, Result} from '../src';
 
 const {some, none} = option;
 const {ok, error} = result;
-const {matchI} = adt;
+const {match, matchI} = adt;
+
+describe('match works correctly', () => {
+  test('match works correctly for option', () => {
+    const someString = some('str');
+    const someStringRes: string = match({
+      Some: ({value}) => value,
+      None: () => 'nothing',
+    })(someString);
+
+    const noString: Option<string> = none();
+    const noStringRes = match({
+      Some: (value) => value,
+      None: () => 'nothing',
+    })(noString);
+
+    expect(someStringRes).toEqual('str');
+    expect(noStringRes).toEqual('nothing');
+  });
+
+  test('match works correctly for result', () => {
+    const okString: Result<string, string> = ok('ok string');
+    const okStringRes = match({
+      Ok: ({value}) => value,
+      Error: ({value}) => value,
+    })(okString);
+
+    const errorString: Result<string, string> = error('error string');
+    const errorStringRes = match({
+      Ok: ({value}) => value,
+      Error: ({value}) => value,
+    })(errorString);
+
+    expect(okStringRes).toEqual('ok string');
+    expect(errorStringRes).toEqual('error string');
+  });
+});
 
 describe('matchI works correctly', () => {
   test('matchI works correctly for option', () => {
